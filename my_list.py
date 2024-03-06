6ab6c890052908108c3a189dfa009412aee88f7c
Cynthia13K <161454141+Cynthia13K@users.noreply.github.com>
2024-03-06 18:55:11 +0300

    Add files via upload

---
+++ my_list.py
@@ -0,0 +1,22 @@
+my_list[]
+ 
+#append
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+#insert
+my_list.insert(1,15)
+
+#extend
+my_list.extend(50,60,70)
+
+#remove
+my_list.pop()
+
+#sort
+my_list.sort()
+
+index_30 = my_list.index(30)
+print("Index of 30 in my_list:", index_30)
